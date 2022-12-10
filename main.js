ad8cfaa339e0620680bee698a2d6f585f61d907e
Will <wlasala@hotmail.com>
2022-12-10 04:53:18 +0000

    removed form for debugging

--- main.js
+++ main.js
@@ -58,8 +58,8 @@ async function loadData(){
       ${address.house_num} ${address.street}, <br/>
       ${address.city}, ${address.state} <br/>
       <p>${removeMakerLink}</p>
-      ${getStarComponent(house.id, avgStarRating)}
-      ${inputForm(house.id)}`;
+      ${getStarComponent(house.id, avgStarRating)}`;
+      // ${inputForm(house.id)}`;
 
     newMapMarkerCounter = (house.id > newMapMarkerCounter)?house.id:newMapMarkerCounter;
 
